0df176c1e6302146c96361a539be716fb13f039b
gargiPunyani <gargipunyani1@gmail.com>
2024-08-31 10:45:18 +0530

    login

--- client/TravelTourism/src/pages/login/index.tsx
+++ client/TravelTourism/src/pages/login/index.tsx
@@ -88,7 +88,7 @@ const Login = () => {
           });
           document.cookie = "token=" + response.data.token;
           notify();
-          navigate('/', { replace: true });
+          navigate('/home', { replace: true });
           localStorage.setItem('token', response.data.token);
         }
       } catch (error) {
