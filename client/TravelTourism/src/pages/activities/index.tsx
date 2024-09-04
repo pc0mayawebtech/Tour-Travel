22a7b1155ab8a1236c5edd2be084c6b07f8b8aed
pc0mayawebtech <mayawebtech2@gmail.com>
2024-09-04 18:59:54 +0530

    fix activity bug

--- client/TravelTourism/src/pages/activities/index.tsx
+++ client/TravelTourism/src/pages/activities/index.tsx
@@ -16,26 +16,27 @@ export const Activity = () => {
         <h1>Find your next Adventure</h1>
         <span>Bringing you the best activities from across the world</span>
       </div>
-        <div className="container">
-          <div className="row">
-            <div className="activityhappeningAround">
-              <h3>Top activities happening around you</h3>
-            </div>
+      <div className="container">
+        <div className="row">
+          <div className="activityhappeningAround">
+            <h3>Top activities happening around you</h3>
           </div>
-          <div className="row">
-            <div>
-              <Index />
-            </div>
-          </div>
+        </div>
+        <div className="row">
           <div>
-            <div className="choosingUs mb-4">
-              <h3>Why choose agoda??</h3>
-            </div>
-            <div className="row chosingUs">
-              {ChooseUs.map((item, id) => {
-                return (
-                  <div className="col-lg-4 col-md-12 col-sm-12 col-12 whyChooseUs" key={id}>
-                    <div className="">
+            <Index />
+          </div>
+        </div>
+        <div>
+          <div className="choosingUs mb-4">
+            <h3>Why choose agoda??</h3>
+          </div>
+          <div className="row">
+            {ChooseUs.map((item, id) => {
+              return (
+                <div className="col-lg-4 col-md-12 col-sm-12 col-12" key={id}>
+                  <div className="chosingUs">
+                    <div className="whyChooseUs">
                       <div>
                         <img src={item.img} />
                       </div>
@@ -45,53 +46,54 @@ export const Activity = () => {
                       </div>
                     </div>
                   </div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="discoverMore">
-            <h3>Discover more places around</h3>
-            <div className="discoverMorePlacesSlide">
-              <Slick />
-            </div>
-          </div>
-          <div className="outsideIndia">
-            <div>
-              <h3>Discover More Places Outside India</h3>
-            </div>
-            <div>
-              <Destinationoutisdeindia />
-            </div>
-          </div>
-
-          <div className="customerReview">
-            <div className="reviewsAboutServices">
-              <h3>Overheard from travelers</h3>
-            </div>
-            <div className="row reviewsByCustomer">
-              <Flightreview
-                hotelname="Hotel Granvia Kyoto"
-                place="in Japan"
-                name="Diane from the United States"
-                text="Our family was traveling via bullet train between cities in
-                 Japan with our luggage - the location for this hotel made"
-              />
-              <Flightreview
-                hotelname="Novotel Rotorua Lakeside "
-                place="in New Zealand"
-                name=" Michael from Australia"
-                text='"Started with stress, ended with joy.
-                 Thanks Agoda."'
-              />
-              <Flightreview
-                hotelname="Fairmont Singapore"
-                place="in Singapore"
-                name="Malvin from Singapore"
-                text="The hotel was simple amazing and I couldn't thank Agoda more for helping out."
-              />
-            </div>
+                </div>
+              );
+            })}
           </div>
         </div>
+        <div className="discoverMore">
+          <h3>Discover more places around</h3>
+          <div className="discoverMorePlacesSlide">
+            <Slick />
+          </div>
+        </div>
+        <div className="outsideIndia">
+          <div>
+            <h3>Discover More Places Outside India</h3>
+          </div>
+          <div>
+            <Destinationoutisdeindia />
+          </div>
+        </div>
+
+        <div className="customerReview">
+          <div className="reviewsAboutServices">
+            <h3>Overheard from travelers</h3>
+          </div>
+          <div className="row reviewsByCustomer">
+            <Flightreview
+              hotelname="Hotel Granvia Kyoto"
+              place="in Japan"
+              name="Diane from the United States"
+              text="Our family was traveling via bullet train between cities in
+                 Japan with our luggage - the location for this hotel made"
+            />
+            <Flightreview
+              hotelname="Novotel Rotorua Lakeside "
+              place="in New Zealand"
+              name=" Michael from Australia"
+              text='"Started with stress, ended with joy.
+                 Thanks Agoda."'
+            />
+            <Flightreview
+              hotelname="Fairmont Singapore"
+              place="in Singapore"
+              name="Malvin from Singapore"
+              text="The hotel was simple amazing and I couldn't thank Agoda more for helping out."
+            />
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
